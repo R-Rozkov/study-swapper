f70a32beda136542e2f3946035355d1673d6ed3b
R-Rozkov <137500276+R-Rozkov@users.noreply.github.com>
2023-06-25 23:23:23 +0300

    Update ethSwap.sol
    add implement

--- ethSwap.sol
+++ ethSwap.sol
@@ -6,4 +6,12 @@ contract EthSwap {
     string public name = "EthSwap Instant Exchange";
     Token public token;
     uint public rate = 100;
+    constructor(Token _token) public {
+        token = _token;
+    }
+
+    function buyTokens() public payable {
+        uint tokenAmount = msg.value * rate;
+        token.transfer(msg.sender, tokenAmount);
+    }
 }
