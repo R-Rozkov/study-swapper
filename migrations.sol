74c7639750a473612c0320c6f6add0e33ed39d81
R-Rozkov <137500276+R-Rozkov@users.noreply.github.com>
2023-06-28 16:03:50 +0300

    Update migrations.sol

--- migrations.sol
+++ migrations.sol
@@ -14,7 +14,7 @@ contract Migrations {
   }
 
   function upgrade(address new_address) public restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
+    Migrations upgraded = Migrations(12323);
+    upgraded.setCompleted(new_address);
   }
 }
